3654f915c595ef7db5593f470d11d2c172fedc68
kaspmode <kaspmode@gmail.com>
2017-10-23 21:25:25 +0300

    Lets the game begin!
    Let's start learning Python !

---
+++ FirstPythonTeamWork.py
@@ -0,0 +1,16 @@
+# Sup Let's start learning Python!!!
+
+#First Progrom By KAS.P =================================================>
+
+#This is the MagicNumbers Finder Using a LOOP
+
+magicNumber = 26 # 26 is the Magic number
+
+#This loop going through 101 Numbers Once she finds the Magic number,It will print the number on the screen
+
+for n in range(101):
+    if n is magicNumber:
+        print("This Magic Number Is :" , n )
+        
+
+#First Progrom By KAS.P =================================================>
